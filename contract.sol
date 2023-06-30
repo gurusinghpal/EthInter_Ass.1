6e2e42eba6bb21b780fe0ba43f5db93d5f2448cc
Guru Singh <127821747+gurusinghpal@users.noreply.github.com>
2023-06-30 19:12:51 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AssertionContract {
+contract SmartContract {
     uint public value;
     
-    function setValue(uint _value) external {
+    function requireSet (uint _value) external {
         // Using require() to check a condition
         require(_value > 0, "Value must be greater than zero");
         
         value = _value;
     }
     
-    function incrementValue(uint _amount) external {
+    function assertIncrement (uint _amount) external {
         // Using assert() to check an invariant
         uint newValue = value + _amount;
         assert(newValue > value);
@@ -19,7 +19,7 @@ contract AssertionContract {
         value = newValue;
     }
     
-    function decrementValue(uint _amount) external {
+    function revertDecrement (uint _amount) external {
         // Using revert() to revert the transaction
         if (_amount > value) {
             revert("Amount exceeds current value");
